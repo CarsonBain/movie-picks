e486a7db651d711f347cccceb55692e4a768f027
my name <carson@felixforyou.ca>
2021-04-15 09:21:15 -0400

    fix: remove duplicate row

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -106,38 +106,6 @@
         ></MovieCard>
       </div>
     </section>
-    <section class="mt-20">
-      <div class="flex items-center justify-between mb-6 px-6 md:px-16">
-        <h2 class="text-xl md:text-xl font-bold">Recently added</h2>
-        <router-link
-          to="/all-picks"
-          class="text-gray-100 flex items-center space-x-1 border-b border-gray-200 text-sm"
-          ><span>See All</span
-          ><svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-4 w-4"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </router-link>
-      </div>
-      <div
-        class="grid grid-cols-feature-four-column gap-6 overflow-x-auto px-6 md:px-16"
-      >
-        <MovieCard
-          v-for="recentPick in recentPicks"
-          :key="recentPick.id"
-          :movie="recentPick"
-          :showImages="true"
-        ></MovieCard>
-      </div>
-    </section>
   </div>
 </template>
 <script>
